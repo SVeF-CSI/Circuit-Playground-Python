af41ace20af934d338d4b10a7b1bb1659aace210
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-18 22:47:51 -0700

    Create lab_11a_neopixel_spin.py

---
+++ lab_11a_neopixel_spin.py
@@ -0,0 +1,18 @@
+import board
+import neopixel
+import time
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+led.brightness = 0.1
+
+while True:
+    #each time the for loop runs, i changes from 0 to 1 to 2 to 3...all the way to 9
+    #i is used to select which LED light to turn on/off
+    for i in range(10):
+        led[i] = (255, 0, 0)
+        time.sleep(0.1)
+        led[i] = (0,0,0)
+
+
+
