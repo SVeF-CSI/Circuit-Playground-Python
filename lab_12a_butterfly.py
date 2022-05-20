1dd1a740b31f099cd4b0c40358f39daab9ff882a
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-20 12:31:10 -0700

    Create lab_12a_butterfly.py

---
+++ lab_12a_butterfly.py
@@ -0,0 +1,14 @@
+import board
+import neopixel
+import time
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+led.brightness = 0.1
+
+while True:
+    #down
+    for i in range(5):
+        led[i] = (255, 0, 0)
+        time.sleep(0.1)
+        led[i] = (0,0,0)
