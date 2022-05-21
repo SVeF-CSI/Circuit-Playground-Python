877a37dbc82227f63e63994c5a20114262444cbb
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-20 22:48:53 -0700

    Update lab_16b_button_switch.py

--- lab_16b_button_switch.py
+++ lab_16b_button_switch.py
@@ -1,7 +1,14 @@
 from adafruit_circuitplayground import cp
 
 while True:
-    if cp.button_a:
-        print("pressed a")
-    if cp.button_b:
-        print("pressed b")
+    if cp.button_a:    #pressed
+	    #loop until cp.button changes to False - released)
+        while (cp.button_a):  
+		    pass
+        print("You clicked the button a") #register a click
+    if cp.button_b:    #pressed
+	    #loop until cp.button changes to False - released)
+        while (cp.button_b):  
+		    pass
+        print("You clicked the button b") #register a click
+
