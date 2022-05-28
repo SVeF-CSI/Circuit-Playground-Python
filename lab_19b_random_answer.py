bdecf0ff6453ae30e205be2ee5f49ea97b004741
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-28 16:11:51 -0700

    Create lab_19b_random_answer.py

---
+++ lab_19b_random_answer.py
@@ -0,0 +1,8 @@
+import random
+
+
+answerbank = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes – definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.' 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+
+
+random_answer = random.choice(answerbank)
+print(random_answer)
