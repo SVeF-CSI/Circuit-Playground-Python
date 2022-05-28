c4e00b496ffb56ef6a21fb1b16458b4b59cf1e84
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-28 16:09:03 -0700

    Create lab_18_play_twinkle_little_star.py

---
+++ lab_18_play_twinkle_little_star.py
@@ -0,0 +1,58 @@
+from adafruit_circuitplayground import cp
+
+# cp.play_tone(262, .5) # note c
+# cp.play_tone(294, .5) # note d
+# cp.play_tone(330, .5) # note e
+# cp.play_tone(349, .5) # note f
+# cp.play_tone(392, 1) # note g
+# cp.play_tone(440, 1) # note a
+# cp.play_tone(494, 1) # note b
+# cp.play_tone(523, 1) # note c
+
+cp.play_tone(262, .5) # note c
+cp.play_tone(262, .5) # note c
+cp.play_tone(392, .5) # note g    
+cp.play_tone(392, .5) # note g
+cp.play_tone(440, .5) # note a
+cp.play_tone(440, .5) # note a
+cp.play_tone(392, 1) # note g 
+
+cp.play_tone(349, .5) # note f
+cp.play_tone(349, .5) # note f
+cp.play_tone(330, .5) # note e
+cp.play_tone(330, .5) # note e
+cp.play_tone(294, .5) # note d
+cp.play_tone(294, .5) # note d
+cp.play_tone(262, 1) # note c
+
+cp.play_tone(392, .5) # note g
+cp.play_tone(392, .5) # note g
+cp.play_tone(349, .5) # note f
+cp.play_tone(349, .5) # note f
+cp.play_tone(330, .5) # note e
+cp.play_tone(330, .5) # note e
+cp.play_tone(294, 1) # note d
+
+cp.play_tone(392, .5) # note g
+cp.play_tone(392, .5) # note g
+cp.play_tone(349, .5) # note f
+cp.play_tone(349, .5) # note f
+cp.play_tone(330, .5) # note e
+cp.play_tone(330, .5) # note e
+cp.play_tone(294, 1) # note d
+
+cp.play_tone(262, .5) # note c
+cp.play_tone(262, .5) # note c
+cp.play_tone(392, .5) # note g    
+cp.play_tone(392, .5) # note g
+cp.play_tone(440, .5) # note a
+cp.play_tone(440, .5) # note a
+cp.play_tone(392, 1) # note g 
+
+cp.play_tone(349, .5) # note f
+cp.play_tone(349, .5) # note f
+cp.play_tone(330, .5) # note e
+cp.play_tone(330, .5) # note e
+cp.play_tone(294, .5) # note d
+cp.play_tone(294, .5) # note d
+cp.play_tone(262, 1) # note c
