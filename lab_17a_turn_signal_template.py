79016dfe72bfb48036b941cb345ff571e08d5bae
kc-encorps <105824766+kc-encorps@users.noreply.github.com>
2022-05-28 00:33:46 -0700

    Create lab_17a_turn_signal_template.py

---
+++ lab_17a_turn_signal_template.py
@@ -0,0 +1,16 @@
+from adafruit_circuitplayground import cp
+
+while True:
+    if cp.button_a==True:
+        while(cp.button_a==True):
+            pass
+        # click detected
+
+
+
+
+    if cp.button_b==True:
+        while(cp.button_b==True):
+            pass
+        #click detected
+
